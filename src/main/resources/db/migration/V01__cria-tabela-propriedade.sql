848f03d2d3f69869bceac0ce5e6f1b7314f431d3
wyslan alves <wyslandasilvaalves@gmail.com>
2023-06-14 18:05:09 -0300

    Primeira migration Flyway

---
+++ src/main/resources/db/migration/V01__cria-tabela-propriedade.sql
@@ -0,0 +1,10 @@
+CREATE TABLE proprietario(
+	id BIGINT NOT NULL AUTO_INCREMENT,
+	nome VARCHAR(60) NOT NULL,
+	email VARCHAR(255) NOT NULL,
+	telefone VARCHAR(20) NOT NULL,
+	
+	PRIMARY KEY (id)
+);
+ALTER  TABLE proprietario
+ADD CONSTRAINT uk_proprietario UNIQUE (email);
