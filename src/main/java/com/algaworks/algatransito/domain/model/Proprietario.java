b7a92468f8bff0558d90986c505ccff64a7af882
wyslan alves <wyslandasilvaalves@gmail.com>
2023-06-14 18:17:47 -0300

    Mapeando entidade com Jakarta Persistence

--- src/main/java/com/algaworks/algatransito/domain/model/Proprietario.java
+++ src/main/java/com/algaworks/algatransito/domain/model/Proprietario.java
@@ -1,12 +1,19 @@
 package com.algaworks.algatransito.domain.model;
 
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
 public class Proprietario {
 
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
