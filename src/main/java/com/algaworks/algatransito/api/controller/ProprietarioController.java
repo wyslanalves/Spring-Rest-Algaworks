b91f3f252e3f2cdcbea6484d09ccc334bff6338a
wyslan alves <wyslandasilvaalves@gmail.com>
2023-06-15 09:36:00 -0300

    Implementando Domain Services

--- src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
+++ src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
@@ -1,25 +1,59 @@
 package com.algaworks.algatransito.api.controller;
 
 import com.algaworks.algatransito.domain.model.Proprietario;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.TypedQuery;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Arrays;
+import com.algaworks.algatransito.domain.repository.ProprietarioRepository;
+import com.algaworks.algatransito.domain.service.RegistroProprietarioService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+
+@AllArgsConstructor
 @RestController
+@RequestMapping("/proprietarios")
 public class ProprietarioController {
 
-    @PersistenceContext
-    private EntityManager entityManager;
+    private final ProprietarioRepository proprietarioRepository;
+    private final RegistroProprietarioService registroProprietarioService;
 
-    @GetMapping("/proprietarios")
+    @GetMapping
     public List<Proprietario> listar(){
+        return proprietarioRepository.findAll();
+    }
 
-        return entityManager.createQuery("from Proprietario", Proprietario.class).getResultList();
+    @GetMapping("/{proprietarioId}")
+    public ResponseEntity<Proprietario> buscar(@PathVariable Long proprietarioId){
+        return  proprietarioRepository.findById(proprietarioId)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Proprietario adicionar(@RequestBody @Valid Proprietario proprietario){
+       return registroProprietarioService.salvar(proprietario);
+    }
+
+    @PutMapping("/{proprietarioId}")
+    public ResponseEntity<Proprietario> atualizar(@PathVariable Long proprietarioId,
+                                                  @RequestBody Proprietario proprietario){
+        if(!proprietarioRepository.existsById(proprietarioId)){
+            return ResponseEntity.notFound().build();
+        }
+        proprietario.setId(proprietarioId);
+        Proprietario proprietarioAtualizar =  registroProprietarioService.salvar(proprietario);
+        return ResponseEntity.ok(proprietarioAtualizar);
+    }
+
+    @DeleteMapping("/{proprietarioId}")
+    public ResponseEntity<Void> remover(@PathVariable Long proprietarioId){
+        if(!proprietarioRepository.existsById(proprietarioId)){
+            return ResponseEntity.notFound().build();
+        }
+        registroProprietarioService.excluir(proprietarioId);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/algaworks/algatransito/domain/model/Proprietario.java
+++ src/main/java/com/algaworks/algatransito/domain/model/Proprietario.java
@@ -1,6 +1,9 @@
 package com.algaworks.algatransito.domain.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,7 +18,17 @@ public class Proprietario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank
+    @Size(max = 60)
     private String nome;
+
+    @NotBlank
+    @Size(max = 255)
+    @Email
     private String email;
+
+    @NotBlank
+    @Size(max = 20)
     private String telefone;
 }
